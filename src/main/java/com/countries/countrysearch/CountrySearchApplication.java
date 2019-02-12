ca9d5a020472a99244f7e826f14c4610400b7b75
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2019-02-11 21:30:24 -0600

    Update CountrySearchApplication.java

--- src/main/java/com/countries/countrysearch/CountrySearchApplication.java
+++ src/main/java/com/countries/countrysearch/CountrySearchApplication.java
@@ -6,11 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CountrySearchApplication
 {
+    static CountryList countriesListed;
 
     public static void main(String[] args)
     {
+        countriesListed = new CountryList();
         SpringApplication.run(CountrySearchApplication.class, args);
     }
-
 }
 
